2635d1b6881dbe151bb7bf657de1e661491bf16d
Festus <ojofestus2030@gmail.com>
2026-04-20 12:13:47 +0100

    Remove commented-out code for clarity in fetchInfo function

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ const fetchInfo = async () =>{
         display.innerHTML += `<p>${element.id}. ${element.login} <br> ${element.node_id} <br> <img src=${element.avatar_url}> <br> ${element.gravatar_id} <br> ${element.url} <br> ${element.html_url} <br> ${element.followers_url} <br> ${element.following_url} <br> ${element.gists_url} <br> ${element.starred_url} <br> ${element.subscriptions_url} <br> ${element.organizations_url} <br> ${element.repos_url} <br> ${element.events_url} <br> ${element.received_events_url} <br> ${element.type} <br> ${element.user_view_type} <br> ${element.site_admin}</p> `
     }
 }
+
+// convertedData.map((element) => {
+    //     display.innerHTML += `<p>${element.id}. ${element.address.city}</p>`
+    //     // `<img style="" src=${element.image_url}/>`
+    // })
